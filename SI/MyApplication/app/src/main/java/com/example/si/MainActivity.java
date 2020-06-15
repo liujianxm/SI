dce2b08dc44ac6fead65f026cf66c10f074e2170
liujianxm <1286237195@qq.com>
2020-06-15 11:01:32 +0800

    Original version

---
+++ SI/MyApplication/app/src/main/java/com/example/si/MainActivity.java
@@ -0,0 +1,386 @@
+package com.example.si;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
+import androidx.loader.content.CursorLoader;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.ContextThemeWrapper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.content.Intent;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.graphics.drawable.Drawable;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private ImageView imageView;
+    private Dialog dialog_pic;
+    private static final int TAKE_PHOTO = 0;
+    private static final int CHOOSE_PHOTO = 1;
+    private Uri photoURI;
+    private String currentPhotoPath;
+    private String currentPicturePath;
+    private File showPic;
+    private String imgPath = "";
+    private String imgName = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = (ImageView) findViewById(R.id.imageview3);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPictureDialog();
+            }
+        });
+
+        button = (Button) findViewById(R.id.image);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPictureSelectDialog();
+            }
+        });
+
+    }
+
+    void openPictureSelectDialog() {
+        Context dialogContext = new ContextThemeWrapper(MainActivity.this, android.R.style.Theme_Light);
+        String[] choiceItems = new String[2];
+        choiceItems[0] = "camera";
+        choiceItems[1] = "album";
+        ListAdapter adapter = new ArrayAdapter<String>(dialogContext, android.R.layout.simple_list_item_1, choiceItems);
+        AlertDialog.Builder builder = new AlertDialog.Builder(dialogContext);
+        builder.setTitle("Add picture");
+        builder.setSingleChoiceItems(adapter, -1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case 0:
+                        checkPermission();
+                        //TakePhoto();
+                        break;
+                    case 1:
+                        PickPhotoFromGallery();
+                        break;
+                }
+                dialog.dismiss();
+            }
+        });
+        builder.create().show();
+    }
+
+    void openPictureDialog() {
+//////////////////////////////notice/////////////////////////////////////////////
+    }
+
+    private void displayForDlg(ImageView imgView, String imgPath2, Button btnBig) {
+        ///////////////////////////////////////////
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case TAKE_PHOTO:
+                    String status = Environment.getExternalStorageState();
+                    if (status.equals(Environment.MEDIA_MOUNTED)) {
+                        //Bitmap bitmap = BitmapFactory.decodeFile(Environment.getExternalStorageDirectory() + "/image.jpg");
+                       try {
+                           Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(photoURI));
+                           int degree = getBitmapDegree(currentPhotoPath);
+                           System.out.println(degree);
+                           Bitmap newBitmap = ImageTools.zoomBitmap(bitmap, bitmap.getWidth() / 5, bitmap.getHeight() / 5);
+                           bitmap.recycle();
+                           newBitmap = rotateBitmapByDegree(newBitmap, degree);
+                           imageView.setImageBitmap(newBitmap);
+                           ImageTools.savePhotoToSDCard(newBitmap, Environment.getExternalStorageDirectory().getAbsolutePath(), String.valueOf(System.currentTimeMillis()));
+                       }catch (FileNotFoundException e){
+                           e.printStackTrace();
+                       }
+                    }
+                    break;
+                case CHOOSE_PHOTO:
+                    ContentResolver resolver = getContentResolver();
+                    Uri originalUri = data.getData();
+                    currentPicturePath = getRealPathFromUri(this,originalUri);
+
+                    try {
+                        Bitmap photo = MediaStore.Images.Media.getBitmap(resolver, originalUri);
+                        int degree = getBitmapDegree(currentPicturePath);
+                        if (photo != null) {
+                            Bitmap smallBitmap = ImageTools.zoomBitmap(photo, photo.getWidth() / 5, photo.getHeight() / 5);
+                            smallBitmap = rotateBitmapByDegree(smallBitmap, degree);
+                            photo.recycle();
+                            imageView.setImageBitmap(smallBitmap);
+                        }
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    private void TakePhoto() {
+        //  Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //  Uri imageUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory(),"image.jpg"));
+        //  intent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            File photoFile = null;
+            //            String photoFilePath = null;
+            try {
+                photoFile = createImageFile();
+                showPic = photoFile;
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            currentPhotoPath = photoFile.getAbsolutePath();
+            if (photoFile != null) {
+                photoURI = FileProvider.getUriForFile(MainActivity.this,
+                        "com.example.si.provider",
+                        photoFile);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(intent, TAKE_PHOTO);
+                Log.v("Camera", "Here we are");
+            }
+        }
+    }
+
+
+    private void PickPhotoFromGallery() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, CHOOSE_PHOTO);
+    }
+
+
+    private String getImageFilePath() {
+        String mCaptureDir = "/storage/emulated/0/C3/cameraPhoto";
+        File dir = new File(mCaptureDir);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        String mCapturePath = mCaptureDir + "/" + "Photo_" + System.currentTimeMillis() + ".jpg";
+        return mCapturePath;
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        //currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    /**
+     * 获取图片的旋转角度
+     *
+     * @param path 图片绝对路径
+     * @return 图片的旋转角度
+     */
+    public static int getBitmapDegree(String path) {
+        int degree = 0;
+        try {
+            // 从指定路径下读取图片，并获取其EXIF信息
+            ExifInterface exifInterface = new ExifInterface(path);
+            // 获取图片的旋转信息
+            int orientation = exifInterface.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+            Log.v("TAG", "原图被旋转角度： ========== " + orientation );
+            switch (orientation) {
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    degree = 90;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    degree = 180;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    degree = 270;
+                    break;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return degree;
+    }
+    /**
+     * 将图片按照指定的角度进行旋转
+     *
+     * @param bitmap 需要旋转的图片
+     * @param degree 指定的旋转角度
+     * @return 旋转后的图片
+     */
+    public static Bitmap rotateBitmapByDegree(Bitmap bitmap, int degree) {
+        // 根据旋转角度，生成旋转矩阵
+        Matrix matrix = new Matrix();
+        matrix.postRotate(degree);
+        // 将原始图片按照旋转矩阵进行旋转，并得到新的图片
+        Bitmap newBitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+        if (bitmap != null && !bitmap.isRecycled()) {
+            bitmap.recycle();
+        }
+        return newBitmap;
+    }
+
+
+    /////////////////////////////////////
+    private void checkPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE}, 0);
+        } else {
+            TakePhoto();
+        }
+    }
+    /**
+     * 根据图片的Uri获取图片的绝对路径。@uri 图片的uri
+     * @return 如果Uri对应的图片存在,那么返回该图片的绝对路径,否则返回null
+     */
+    public static String getRealPathFromUri(Context context, Uri uri) {
+        if(context == null || uri == null) {
+            return null;
+        }
+        if("file".equalsIgnoreCase(uri.getScheme())) {
+            return getRealPathFromUri_Byfile(context,uri);
+        } else if("content".equalsIgnoreCase(uri.getScheme())) {
+            return getRealPathFromUri_Api11To18(context,uri);
+        }
+//        int sdkVersion = Build.VERSION.SDK_INT;
+//        if (sdkVersion < 11) {
+//            // SDK < Api11
+//            return getRealPathFromUri_BelowApi11(context, uri);
+//        }
+////        if (sdkVersion < 19) {
+////             SDK > 11 && SDK < 19
+////            return getRealPathFromUri_Api11To18(context, uri);
+//            return getRealPathFromUri_ByXiaomi(context, uri);
+////        }
+//        // SDK > 19
+        return getRealPathFromUri_AboveApi19(context, uri);//没用到
+    }
+
+    //针对图片URI格式为Uri:: file:///storage/emulated/0/DCIM/Camera/IMG_20170613_132837.jpg
+    private static String getRealPathFromUri_Byfile(Context context,Uri uri){
+        String uri2Str = uri.toString();
+        String filePath = uri2Str.substring(uri2Str.indexOf(":") + 3);
+        return filePath;
+    }
+
+    /**
+     * 适配api19以上,根据uri获取图片的绝对路径
+     */
+    @SuppressLint("NewApi")
+    private static String getRealPathFromUri_AboveApi19(Context context, Uri uri) {
+        String filePath = null;
+        String wholeID = null;
+
+        wholeID = DocumentsContract.getDocumentId(uri);
+
+        // 使用':'分割
+        String id = wholeID.split(":")[1];
+
+        String[] projection = { MediaStore.Images.Media.DATA };
+        String selection = MediaStore.Images.Media._ID + "=?";
+        String[] selectionArgs = { id };
+
+        Cursor cursor = context.getContentResolver().query(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, projection,
+                selection, selectionArgs, null);
+        int columnIndex = cursor.getColumnIndex(projection[0]);
+
+        if (cursor.moveToFirst()) {
+            filePath = cursor.getString(columnIndex);
+        }
+        cursor.close();
+        return filePath;
+    }
+
+    /**
+     * //适配api11-api18,根据uri获取图片的绝对路径。
+     * 针对图片URI格式为Uri:: content://media/external/images/media/1028
+     */
+    private static String getRealPathFromUri_Api11To18(Context context, Uri uri) {
+        String filePath = null;
+        String[] projection = { MediaStore.Images.Media.DATA };
+
+        CursorLoader loader = new CursorLoader(context, uri, projection, null,
+                null, null);
+        Cursor cursor = loader.loadInBackground();
+
+        if (cursor != null) {
+            cursor.moveToFirst();
+            filePath = cursor.getString(cursor.getColumnIndex(projection[0]));
+            cursor.close();
+        }
+        return filePath;
+    }
+
+    /**
+     * 适配api11以下(不包括api11),根据uri获取图片的绝对路径
+     */
+    private static String getRealPathFromUri_BelowApi11(Context context, Uri uri) {
+        String filePath = null;
+        String[] projection = { MediaStore.Images.Media.DATA };
+        Cursor cursor = context.getContentResolver().query(uri, projection,
+                null, null, null);
+        if (cursor != null) {
+            cursor.moveToFirst();
+            filePath = cursor.getString(cursor.getColumnIndex(projection[0]));
+            cursor.close();
+        }
+        return filePath;
+    }
+
+
+}
